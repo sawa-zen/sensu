e685df8367d0ae4805665a0cdcf543a9ba6053f9
sawada <sawadasuiren@gmail.com>
2015-08-09 04:27:07 +0900

    継承をつかってコードを綺麗に

--- sensu.jquery.js
+++ sensu.jquery.js
@@ -3,98 +3,126 @@
   var pluginName = 'sensu',
       defaults = {};
 
-  // コンストラクタ
+  /**
+   * メインクラス
+   */
   function Sensu(element, options) {
-    this.element = element;
     this.settings = $.extend({}, defaults, options);
     this._defaults = defaults;
-    this._name = pluginName;
     this._stage = null;
-    this.currentPage = 0;
     this.init();
   }
+  Sensu.prototype.init = function() {
+    var _this = this;
 
-  // メソッド
-  Sensu.prototype = {
+    // createjsのステージを用意
+    _this._stage = new createjs.Stage('sensu');
 
-    /**
-     * 初期設定
-     */
-    init: function() {
-      var _this = this;
+    // スライダーを生成
+    var slider = new Slider();
+    _this._stage.addChild(slider);
 
-      // createjsのステージを用意
-      _this._stage = new createjs.Stage('sensu');
+    // canvasの描画設定
+    // 30fpsで描画を繰り返す
+    createjs.Ticker.setFPS(30);
+    createjs.Ticker.addEventListener('tick', function () {
+      _this._stage.update();
+    });
 
-      // LoadQueueクラス
-      var loadQueue = new createjs.LoadQueue();
-      loadQueue.addEventListener('fileload', handleFileLoadComplete);
-
-      function handleFileLoadComplete(event) {
-        // 読み込んだファイル
-        console.info(event.result);
-        _this._addImage(event.result);
-      }
-
-      // canvasの描画設定
-      // 30fpsで描画を繰り返す
-      createjs.Ticker.setFPS(30);
-      createjs.Ticker.addEventListener('tick', function () {
-        _this._stage.update();
-      });
-
-      // 読み込み開始
-      loadQueue.loadManifest(_this.settings.list);
-    },
+    // LoadQueueクラス
+    var loadQueue = new createjs.LoadQueue();
+    // 一つのファイル毎のcallback
+    loadQueue.addEventListener('fileload', function(event) {
+      // スライダーにページを追加
+      slider.addPage(event.result);
+    });
+    // 読み込み開始
+    loadQueue.loadManifest(_this.settings.list);
+  };
 
 
-    /**
-     * 画像の追加
-     */
-    _addImage: function(img) {
-      var _this = this;
-      var imgWidth = img.width,
-          imgHeight = img.height,
-          sliceCount = 6,
-          sliceWidth = imgWidth / sliceCount,
-          degToRad = Math.PI / 180;
 
-      console.info(sliceWidth);
+  /**
+   * スライダークラス
+   */
+  function Slider() {
+    this.Container_constructor();
+    this.initialize();
+  }
+  // Containerクラスを継承
+  createjs.extend(Slider, createjs.Container);
+  // 初期化
+  Slider.prototype.initialize = function() {
+  };
+  // ページ追加
+  Slider.prototype.addPage = function(img) {
+    this.addChild(new Page(img));
+  };
+  createjs.promote(Slider, 'Container');
 
-      var container = new createjs.Container();
 
-      for (var i = 0; i < sliceCount; i++) {
-        var slice = new createjs.Bitmap(img);
-        slice.sourceRect = new createjs.Rectangle(sliceWidth * i, 0, sliceWidth, imgHeight);
-        slice.cache(0, 0, sliceWidth, imgHeight);
-        slice.filters = [new createjs.ColorMatrixFilter(new createjs.ColorMatrix())];
-        container.addChild(slice);
-      }
 
-      _this._stage.addChild(container);
+  /**
+   * ページクラス
+   */
+  function Page(img) {
+    this.Container_constructor();
+    this.x = 415;
+    this.width = img.width;
+    this.height = img.height;
+    this.sliceCount = 10;
+    this.sliceWidth = this.width / this.sliceCount;
+    this.initialize(img);
+  }
+  // Containerクラスを継承
+  createjs.extend(Page, createjs.Container);
+  // 初期化
+  Page.prototype.initialize = function(img) {
 
-      function updateEffect(value) {
-        var l = container.getNumChildren();
-        for (var i = 0; i < l; i++) {
-          var slice = container.getChildAt(i);
-          slice.y = Math.sin(value * degToRad) * -sliceWidth / 2;
-          if (i % 2) {
-            slice.skewY = value;
-          } else {
-            slice.skewY = -value;
-            slice.y -= sliceWidth * Math.sin(slice.skewY * degToRad);
-          }
-          slice.x = sliceWidth * (i - l / 2) * Math.cos(slice.skewY * degToRad);
-          slice.filters[0].matrix.setColor(Math.sin(slice.skewY * degToRad) * -80);
-          slice.updateCache();
-        }
-        _this._stage.update();
-      }
-
-      updateEffect(50);
+    // スライス数分画像を分割して生成
+    for (var i = 0; i < this.sliceCount; i++) {
+      var slice = new createjs.Bitmap(img);
+      slice.sourceRect = new createjs.Rectangle(
+        this.sliceWidth * i, 0, this.sliceWidth, this.height
+      );
+      slice.cache(0, 0, this.sliceWidth, this.height);
+      slice.filters = [new createjs.ColorMatrixFilter(new createjs.ColorMatrix())];
+      this.addChild(slice);
     }
 
+    this.open();
   };
+  // 開く
+  Page.prototype.open = function() {
+    var value = 45;
+    degToRad = Math.PI / 180;
+    var l = this.getNumChildren();
+    for (var i = 0; i < l; i++) {
+      var slice = this.getChildAt(i);
+      slice.y = Math.sin(value * degToRad) * -this.sliceWidth / 2;
+      if (i % 2) {
+        slice.skewY = value;
+      } else {
+        slice.skewY = -value;
+        slice.y -= this.sliceWidth * Math.sin(slice.skewY * degToRad);
+      }
+      slice.x = this.sliceWidth * (i - l / 2) * Math.cos(slice.skewY * degToRad);
+      slice.filters[0].matrix.setColor(Math.sin(slice.skewY * degToRad) * -80);
+      slice.updateCache();
+    }
+  };
+  // 閉じる
+  Page.prototype.close = function() {
+  };
+  createjs.promote(Page, 'Container');
+
+
+
+  /**
+   * コントローラークラス
+   */
+
+
 
   // メイン
   $.fn[pluginName] = function(options) {
